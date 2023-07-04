2c6e34a0c2f413908b8f563ce6de9f86b8b8fd05
= <=>
2023-07-04 12:59:29 -0300

    fé

---
+++ WebProjectMyHouseBeach/src/java/Cliente/Cliente.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Cliente;
+
+/**
+ *
+ * @author aluno
+ */
+public class Cliente {
+    String nome;
+    String email;
+    String cpf;
+    String genero;
+    String login;
+    String senha;
+    
+    
+}
